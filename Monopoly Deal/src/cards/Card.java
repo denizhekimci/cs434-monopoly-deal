6e83c607c362dd75e6c00df2f573427b3178d998
denizhekimci <deniz.hekimci@ozu.edu.tr>
2014-06-06 10:41:22 +0300

    removed Monopoly Deal first version

--- Monopoly Deal/src/cards/Card.java
+++
@@ -1,9 +0,0 @@
-package cards;
-
-public abstract class Card {
-	public abstract void returnToSource();
-	public abstract void draw();
-	public abstract String source();
-	public abstract void setSource(Deck source);
-}
-
